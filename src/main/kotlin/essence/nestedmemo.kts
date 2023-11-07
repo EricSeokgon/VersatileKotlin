68020a3c477c062b40347869080a02223ec9be15
hadesyi <leesk55@gmail.com>
2023-11-07 14:51:37 +0900

    멀티라인 문자열

--- src/main/kotlin/essence/nestedmemo.kts
+++ src/main/kotlin/essence/nestedmemo.kts
@@ -3,7 +3,7 @@ fun createMemoFor(name: String): String {
         val memo = """Dear $name, a quick reminder about the 
             |party we have scheduled next Tuesday at
             |the 'Low ceremony Cafe's at Noon. | Plaese to...""".trimMargin()
-        return memo
+        return memo.trimMargin()
     }
     return ""
 }
